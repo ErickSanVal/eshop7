32e2a43e0bd33560631c953077e222a1d662485f
Erick <erick.sanchez.valenzuela@gmail.com>
2015-05-04 19:55:42 +0200

    Para tenerlo

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -1,10 +1,10 @@
 class Tshirt < ActiveRecord::Base
 	belongs_to :manufacturer
-	validates_presence_of :manufacturer, :size, :club, :country, :price#, :image
+	validates_presence_of :manufacturer, :size, :club, :country, :image, :price
 	validates_numericality_of :price
-  #has_attached_file :image, styles: { :medium => "125x125>", :thumb => "35x35>" }
-  #validates_attachment :image,
-  #:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+  has_attached_file :image, styles: { :medium => "125x125>", :thumb => "35x35>" }
+  validates_attachment :image,
+  :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 
   def self.latest(num)
     all.order("tshirts.id desc").includes(:manufacturer).limit(num)
