2bbda8cb74469acdca28547f1518117cc4daa77d
KevValle <kevinvalle1@gmail.com>
2015-05-18 18:52:38 +0200

    Añadido test de usuario

--- test/models/user_session_test.rb
+++ test/models/user_session_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+#require 'test_helper'
 
-class UserSessionTest < ActiveSupport::TestCase
+#class UserSessionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+#send
