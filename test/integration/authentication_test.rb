d90c7e97845b3339a751c8fe18342b4c93981cf2
KevValle <kevinvalle1@gmail.com>
2015-05-18 18:59:01 +0200

    Añadido test de autenticacion

---
+++ test/integration/authentication_test.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AuthenticationTest < ActionDispatch::IntegrationTest
+
+  def setup
+    User.create(:name => 'Kevin Valle', :login => 'kevin', :email => 'kevin@emporium.com',
+                :password => 'valle', :password_confirmation => 'valle')
+  end
+
+  test "successful_login" do
+    kevin = new_session_as(:kevin)
+    kevin.tries_to_go_to_admin
+    kevin.logs_in_succesfully("kevin", "valle")
+  end
+
+  test "failed_login" do
+    troli = new_session_as(:troli)
+    troli.tries_to_go_to_admin
+    troli.fails_login("troli", "miguel")
+  end
+
+  private
+
+  module BrowsingTestDSL
+    include ERB::Util
+    attr_writer :name
+
+    def tries_to_go_to_admin
+      get '/admin/book/new'
+      assert_response :redirect
+      assert_redirected_to '/user_sessions/new'
+    end
+
+    def logs_in_succesfully(login, password)
+      post_login(login, password)
+      assert_response :redirect
+      assert_redirected_to '/admin/book/new'
+    end
+
+    def fails_login(login, password)
+      post_login(login, password)
+      assert_response :success
+      assert_template 'user_sessions/new'
+      assert_tag :tag => 'div', :attributes => { :id => 'errorExplanation' }
+      assert_tag :tag => 'li', :content => 'Login is not valid'
+    end
+
+    private
+
+    def post_login(login, password)
+      post '/user_sessions/create', :user_session => { :login => login, :password => password }
+    end
+  end
+
+  def new_session_as(name)
+    open_session do |session|
+      session.extend(BrowsingTestDSL)
+      session.name = name
+      yield session if block_given?
+    end
+  end
+end
